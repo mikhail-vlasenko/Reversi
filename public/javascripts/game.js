3fc712fb9ce997721a5ffb5b5151b6c4c6be6b33
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2021-01-15 20:10:21 +0300

    makeMove updates the field (no animation flags)

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,15 +1,32 @@
-function Game () {
-    this.x = new Array(8); // TODO: make 2d
-    this.player = null; // who is playing on this side
+function Game (player) {
+    this.field = new Array(8);  // 0 - empty, 1 - blue, 2 - red
+    this.player = player; // who is playing on this side (1 - blue player, 2 - red player)
     this.scoreBlue = null;
     this.scoreRed = null;
 
     this.reset = function (){
-        // TODO: reset field and other vars
+        for (let i = 0; i < this.field.length; i++) {
+            this.field[i] = new Array(8).fill(0);
+        }
+        this.scoreBlue = 0;
+        this.scoreRed = 0;
     };
 
     this.makeMove = function(player, x, y) {
-        // TODO: update the field with the move, big function
+        for (let dy = -1; dy <= 1; dy++) {
+            for (let dx = -1; dx <= 1; dx++) {
+                if (dy === 0 && dx === 0){
+                    break;
+                }
+                let i = y + dy;
+                let j = x + dx;
+                while (this.field[i][j] === (3 - player)) {  // while disk === opponent's color
+                    this.field[i][j] = player;
+                    i += dy;
+                    j += dx;
+                }
+            }
+        }
     };
 
     this.availableMoves = function() {
