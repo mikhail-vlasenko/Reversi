675202331dd033498f9269ff4db9ed1503492ed0
vladimirmak02 <vovamak107@gmail.com>
2021-01-15 17:19:58 +0100

    installed websockets

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
+
+//--------------------vvvvv   WEBSOCKETS    vvvv---------------------
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
