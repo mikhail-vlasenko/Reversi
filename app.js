18916a544358ddc440bbed39e984d5bb54feb234
vladimirmak02 <vovamak107@gmail.com>
2021-01-16 15:45:37 +0100

    United www and app.js, www is now not needed at all, keeping for now as a backup

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -15,13 +19,12 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
 
-//--------------------vvvvv   WEBSOCKETS    vvvv---------------------
+//--------------------vvvvv   WEBSOCKET    vvvv---------------------
 
 
 
@@ -41,4 +44,89 @@ app.use(function(err, req, res) {
   res.render('error');
 });
 
-module.exports = app;
+var debug = require('debug')('reversi:server');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+// eslint-disable-next-line no-undef
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      // eslint-disable-next-line no-undef
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      // eslint-disable-next-line no-undef
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
