776d2ed20f831b852a49c1d8c683663879434760
vladimirmak02 <vovamak107@gmail.com>
2021-01-17 11:23:54 +0100

    Fixed logging public assets

--- app.js
+++ app.js
@@ -17,14 +17,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 204 is no-content, so we don't send a favicon
+app.get('/favicon.ico', (req, res) => res.status(204).send());
+// Every route thing below logger will be logged, so favicon and /public/* won't be logged
+app.use(logger('dev'));
 app.use('/', indexRouter);
-
-app.get('/favicon.ico', (req, res) => res.status(204));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
