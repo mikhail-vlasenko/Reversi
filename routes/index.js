5e1568cd65387cf21da344049ec080b44300c28b
vladimirmak02 <vovamak107@gmail.com>
2021-01-17 22:05:16 +0100

    Added statistics to ejs templating engine

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('splash', { title: 'Reversi' });
-});
-
 /* Pressing the 'PLAY' button, returns this page */
 router.get("/play", function(req, res) {
   res.sendFile("game.html", { root: "./public" });
--- statisticsTracker.js
+++ statisticsTracker.js
@@ -2,7 +2,8 @@ var gameStatistics = {
     since: Date.now() /* since we keep it simple and in-memory, keep track of when this object was created */,
     gamesPlayed: 0 /* number of games played */,
     peopleInGame: 0 /* number of games running rn x2 */,
-    blueWinRate: 0.0 /* percentage of games won by blue */
+    blueWins: 0 /* games won by blue */,
+    redWins: 0 /* games won by red */
   };
 
   module.exports = gameStatistics;
